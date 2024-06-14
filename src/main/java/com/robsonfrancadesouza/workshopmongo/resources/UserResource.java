31bd8afc588656a129d249930e3541edde9c48b0
robrfsouza <robrfs@gmail.com>
2024-06-14 18:52:02 -0300

    Obtendo um usuário por id

--- src/main/java/com/robsonfrancadesouza/workshopmongo/resources/UserResource.java
+++ src/main/java/com/robsonfrancadesouza/workshopmongo/resources/UserResource.java
@@ -1,13 +1,14 @@
 package com.robsonfrancadesouza.workshopmongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robsonfrancadesouza.workshopmongo.domain.User;
@@ -26,6 +27,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+		public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 
 }
--- src/main/java/com/robsonfrancadesouza/workshopmongo/service/UserService.java
+++ src/main/java/com/robsonfrancadesouza/workshopmongo/service/UserService.java
@@ -1,21 +1,27 @@
 package com.robsonfrancadesouza.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.robsonfrancadesouza.workshopmongo.domain.User;
 import com.robsonfrancadesouza.workshopmongo.repository.UserRepository;
+import com.robsonfrancadesouza.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+	}
+
+	public User findById (String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
