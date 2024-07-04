9731667c44cef660f4e4934971393b8fd4e7b86e
robrfsouza <robrfs@gmail.com>
2024-07-04 16:21:52 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/robsonfrancadesouza/workshopmongo/resources/UserResource.java
+++ src/main/java/com/robsonfrancadesouza/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,26 +26,31 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-		public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Void> insert (@RequestBody UserDTO userDto){
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDto) {
 		User user = service.fromDTO(userDto);
 		user = service.insert(user);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/robsonfrancadesouza/workshopmongo/service/UserService.java
+++ src/main/java/com/robsonfrancadesouza/workshopmongo/service/UserService.java
@@ -30,7 +30,14 @@ public class UserService {
 		return repo.insert(user);
 	}
 	
+	public void delete (String id) {
+			findById(id);
+			repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
+	
+//end
 }
