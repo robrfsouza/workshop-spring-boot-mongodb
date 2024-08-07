1e9aa35e6e244f3d53677f0fa4a03ea78870d499
robrfsouza <robrfs@gmail.com>
2024-08-07 17:09:19 -0300

    Referenciando os posts do usuario

--- src/main/java/com/robsonfrancadesouza/workshopmongo/config/Instantiation.java
+++ src/main/java/com/robsonfrancadesouza/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.robsonfrancadesouza.workshopmongo.config;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -41,8 +42,12 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("20/07/2024"),  "Partiu viagem", "Vou viajar para Sao Paulo. Abraços!", new AuthorDTO( maria));
 		Post post2 = new Post(null, sdf.parse("21/07/2024"),  "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		
+		userRepository.save(maria);
+		
 	}
 
 }
